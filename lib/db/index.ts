3e5e3e02b57b4b3f92f38e8838647decc6dc8e40
Hauk Storjord <hauks@Hauk-sin-MacBook-Pro.local>
2023-10-05 09:53:03 +0200

    Testing

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,8 @@
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/planetscale-serverless'
-import { connect } from '@planetscale/database'
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+import "dotenv/config";
+
+ 
 
 // create the connection
 const connection = connect({
@@ -9,4 +11,4 @@ const connection = connect({
   password: process.env.DATABASE_PASSWORD
 })
 
-const db = drizzle(connection)
+export const db = drizzle(connection)
